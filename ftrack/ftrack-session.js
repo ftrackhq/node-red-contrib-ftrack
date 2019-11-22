eeafbd1bf1de53700519cecec8fef3eb7e9f80f6
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-11-22 16:23:07 +0100

    code cleanup

--- ftrack/ftrack-session.js
+++ ftrack/ftrack-session.js
@@ -5,11 +5,17 @@ module.exports = function(RED) {
     function FtrackSessionNode(config) {
         RED.nodes.createNode(this,config);
         this.ftrack_config = RED.nodes.getNode(config.server)
-
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });
+        
+        if (this.ftrack_config){
+            this.session = new ftrack.Session(
+                this.ftrack_config.server_url,
+                this.ftrack_config.api_user,
+                this.ftrack_config.api_key
+            )
+            this.session.initializing.then(function () {
+                console.log('API session initialized successfully.');
+            });
+        }
 
     }
     RED.nodes.registerType(
