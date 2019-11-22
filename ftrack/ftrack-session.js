88e42c4ab520fdfd47658539c21006eaee47c772
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-11-22 16:50:52 +0100

    try passing query through connection

--- ftrack/ftrack-session.js
+++ ftrack/ftrack-session.js
@@ -1,12 +1,12 @@
 require('isomorphic-fetch');
 var ftrack = require('ftrack-javascript-api');
 
-
 module.exports = function(RED) {
     function FtrackSessionNode(config) {
         RED.nodes.createNode(this,config);
         this.ftrack_config = RED.nodes.getNode(config.server)
-        
+        var node = this;
+
         if (this.ftrack_config){
             this.session = new ftrack.Session(
                 this.ftrack_config.server_url,
@@ -15,6 +15,14 @@ module.exports = function(RED) {
             )
             this.session.initializing.then(function () {
                 console.log('API session initialized successfully.');
+            });
+
+            this.on('input', function(msg) {
+                console.log('payload', msg.payload)
+                var request = this.session.query(msg.payload);
+                request.then(function(response){
+                    node.send(response.data);
+                })
             });
         }
 
